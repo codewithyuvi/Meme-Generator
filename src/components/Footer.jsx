accb8cf1d07571212779ba9b35876b5ddd401568
Yuvraj Bansal <78293595+codewithyuvi@users.noreply.github.com>
2025-06-14 00:43:32 +0530

    Update Footer.jsx
    Add owner's name at footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Yuvraj Bansal | Copyright ⓒ {year}</p>
     </footer>
   );
 }
